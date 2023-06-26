e5da72e5dc095ccd28114ed24bbfa4143f3d235e
Charley Wright <charleywright06@gmail.com>
2023-06-26 23:36:10 +0100

    Read variables

--- demangle.test.ts
+++ demangle.test.ts
@@ -1,37 +1,22 @@
 import { describe, expect, test } from "@jest/globals";
-import {
-  isMangled,
-  demangle,
-  __EXPORTED_FOR_TESTING_DONT_USE,
-} from "./demangle";
-const { readDenary: _readDenary } = __EXPORTED_FOR_TESTING_DONT_USE;
-
-/*
-Examples of mangled symbols come from here - https://itanium-cxx-abi.github.io/cxx-abi/abi-examples.html#mangling
-and various libraries used in the Apple Music android app
-*/
-
-describe("denary values are parsed properly", () => {
-  test("leading zeroes increase length", () => {
-    expect(_readDenary("000001")).toStrictEqual({ value: 1, length: 6 });
-  });
-});
+import { isMangled, demangle } from "./demangle";
 
 describe("presence of name mangling can be detected", () => {
   test("c functions don't have mangling", () => {
     expect(isMangled("f")).toStrictEqual(false);
-    expect(demangle("f")).toStrictEqual("f");
   });
   test("c++ functions do have mangling", () => {
     expect(isMangled("_Z1fv")).toStrictEqual(true);
-    expect(demangle("_Z1fv")).toStrictEqual("f(void)");
+    expect(isMangled("__Z1fv")).toStrictEqual(true);
   });
 });
 
-describe("parse function names correctly", () => {
-  test("global functions", () => {
-    expect(demangle("_Z2f")).toStrictEqual("_Z2f");
-    expect(demangle("_Z5qrandv")).toBe("qrand(void)");
+describe("parse variables", () => {
+  test("simple variables", () => {
+    expect(demangle("bar")).toBe("bar");
+    expect(demangle("_Z3bar")).toBe("bar");
   });
-  test("namespaced functions", () => {});
+  test("variables with modifiers", () => {
+    expect(demangle("_ZL3bar")).toBe("const bar");
+  });
 });
--- demangle.ts
+++ demangle.ts
@@ -1,12 +1,12 @@
-interface StringRead<ValueType> {
+interface ReadResult<ValueType> {
   value: ValueType;
-  length: number;
+  consumed: number;
 }
 
 /*
 Read a positive integer and 
 */
-type DenaryValue = StringRead<number>;
+type DenaryValue = ReadResult<number>;
 function readDenary(str: string): DenaryValue {
   let numStr = "",
     i = 0;
@@ -14,14 +14,14 @@ function readDenary(str: string): DenaryValue {
     numStr += str[i];
     i++;
   }
-  return { value: parseInt(numStr), length: i };
+  return { value: parseInt(numStr), consumed: i };
 }
 
 enum ArgumentTypes {
   Void = "void",
 }
 
-type FunctionArgumentsValue = StringRead<ArgumentTypes[]>;
+type FunctionArgumentsValue = ReadResult<ArgumentTypes[]>;
 function parseArgumentTypes(str: string): FunctionArgumentsValue {
   let ret: ArgumentTypes[] = [];
   for (let i = 0; i < str.length; i++) {
@@ -31,50 +31,71 @@ function parseArgumentTypes(str: string): FunctionArgumentsValue {
         break;
       }
       default: {
-        return { length: i, value: ret };
+        return { consumed: i, value: ret };
       }
     }
   }
-  return { length: str.length, value: ret };
+  return { consumed: str.length, value: ret };
+}
+
+class Substitution {
+  values: string[] = [];
+
+  push(sub: string) {
+    this.values.push(sub);
+  }
+
+  pop(): string | undefined {
+    const val = this.values.pop();
+    return val;
+  }
+
+  count() {
+    return this.values.length;
+  }
+
+  /*
+    S_ is first
+    S_0 is second
+    S_1 is third
+    ...
+  */
+  get(idx: "_" | number) {
+    return idx === "_" ? this.values[0] : this.values[idx + 1];
+  }
+}
+
+enum Modifiers {
+  Pointer = "P",
+  Const = "K",
+  Reference = "R",
 }
 
 export function isMangled(symbol: string) {
-  return symbol.startsWith("_Z");
+  return symbol.startsWith("_Z") || symbol.startsWith("__Z");
 }
 
 export function demangle(input: string): string {
   if (!isMangled(input)) {
     return input;
   }
-  let symbol = input.substring(2);
+  let symbol = input.substring(input.indexOf("Z") + 1).replace(/ /g, "");
 
-  const nameParts: string[] = [];
-  while (symbol.length > 0) {
-    const partLen = readDenary(symbol);
-    if (
-      isNaN(partLen.value) ||
-      symbol.length < partLen.length + partLen.value
-    ) {
-      break;
-    }
-    nameParts.push(
-      symbol.substring(partLen.length, partLen.length + partLen.value)
-    );
-    symbol = symbol.substring(partLen.length + partLen.value);
-  }
-  if (nameParts.length === 0) {
+  const isConst = symbol[0] === "L";
+  if (isConst) symbol = symbol.substring(1);
+
+  const nameLen = readDenary(symbol);
+  if (isNaN(nameLen.value)) {
     return input;
   }
-
-  const argTypes = parseArgumentTypes(symbol);
-  if (argTypes.length > 0) {
-    let demangled = nameParts.join("::") + `(` + argTypes.value.join(",") + ")";
-    return demangled;
+  symbol = symbol.substring(nameLen.consumed);
+  if (symbol.length < nameLen.value) {
+    return input;
   }
+  const name = symbol.substring(0, nameLen.value);
+  symbol = symbol.substring(nameLen.value);
 
-  return "";
+  let demangled = isConst ? "const " : "";
+  demangled += name;
+  return demangled;
 }
-
-export const __EXPORTED_FOR_TESTING_DONT_USE = {
-  readDenary,
-};
