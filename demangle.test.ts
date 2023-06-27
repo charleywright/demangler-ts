3ee47add16acf7af177548e46542e06d54d6d591
Charley Wright <charleywright06@gmail.com>
2023-06-27 13:20:25 +0100

    Namespace parsing and a little function argument parsing

--- demangle.test.ts
+++ demangle.test.ts
@@ -3,11 +3,11 @@ import { isMangled, demangle } from "./demangle";
 
 describe("presence of name mangling can be detected", () => {
   test("c functions don't have mangling", () => {
-    expect(isMangled("f")).toStrictEqual(false);
+    expect(isMangled("f")).toBe(false);
   });
   test("c++ functions do have mangling", () => {
-    expect(isMangled("_Z1fv")).toStrictEqual(true);
-    expect(isMangled("__Z1fv")).toStrictEqual(true);
+    expect(isMangled("_Z1fv")).toBe(true);
+    expect(isMangled("__Z1fv")).toBe(true);
   });
 });
 
@@ -15,6 +15,7 @@ describe("parse variables", () => {
   test("simple variables", () => {
     expect(demangle("bar")).toBe("bar");
     expect(demangle("_Z3bar")).toBe("bar");
+    expect(demangle("_Z4foo")).toBe("_Z4foo");
   });
   test("variables with modifiers", () => {
     expect(demangle("_ZL3bar")).toBe("const bar");
@@ -22,5 +23,19 @@ describe("parse variables", () => {
   test("variables with namespaces", () => {
     expect(demangle("_ZN1a3barE")).toBe("a::bar");
     expect(demangle("_ZSt3bar")).toBe("std::bar");
+    expect(demangle("_Z1a3bar")).toBe("_Z1a3bar");
+    expect(demangle("_Z3foo4bar")).toBe("_Z3foo4bar");
+    expect(demangle("_ZN4foo14bar2E")).toBe("foo1::bar2");
+  });
+});
+
+describe("parse functions", () => {
+  test("simple functions", () => {
+    expect(demangle("_Z3foov")).toBe("foo(void)");
+    expect(demangle("_Z3foobcahwstijlmxynofdeg")).toBe(
+      "foo(bool,char,signed char,unsigned char,wchar_t,short,unsigned short,int,unsigned int,long,unsigned long,long long,unsigned long long,__int128,unsigned __int128,float,double,long double,__float128)"
+    );
+    expect(demangle("_Z3fooB")).toBe("_Z3fooB");
+    expect(demangle("_Z3fooiB")).toBe("foo(int)"); // TODO: This is somewhat wrong, can we detect this?
   });
 });
--- demangle.ts
+++ demangle.ts
@@ -17,59 +17,177 @@ function readDenary(str: string): DenaryValue {
   return { value: parseInt(numStr), consumed: i };
 }
 
-enum ArgumentTypes {
-  Void = "void",
+type StringValue = ReadResult<string>;
+function readLengthEncoded(str: string): StringValue {
+  const len = readDenary(str);
+  if (isNaN(len.value)) {
+    return { consumed: 0, value: "" };
+  }
+  str = str.substring(len.consumed);
+  if (str.length < len.value) {
+    return { consumed: 0, value: "" };
+  }
+  const value = str.substring(0, len.value);
+  return { value, consumed: len.consumed + len.value };
 }
 
-type FunctionArgumentsValue = ReadResult<ArgumentTypes[]>;
-function parseArgumentTypes(str: string): FunctionArgumentsValue {
-  let ret: ArgumentTypes[] = [];
-  for (let i = 0; i < str.length; i++) {
-    switch (str[i]) {
-      case "v": {
-        ret.push(ArgumentTypes.Void);
+// class Substitution {
+//   values: string[] = [];
+
+//   push(sub: string) {
+//     this.values.push(sub);
+//   }
+
+//   pop(): string | undefined {
+//     const val = this.values.pop();
+//     return val;
+//   }
+
+//   count() {
+//     return this.values.length;
+//   }
+
+//   /*
+//     S_ is first
+//     S_0 is second
+//     S_1 is third
+//     ...
+//   */
+//   get(idx: "_" | number) {
+//     return idx === "_" ? this.values[0] : this.values[idx + 1];
+//   }
+// }
+
+// https://itanium-cxx-abi.github.io/cxx-abi/abi-mangling.html
+enum Type {
+  Void = "void", // v
+  Bool = "bool", // b
+  Char = "char", // c
+  SChar = "signed char", // a
+  UChar = "unsigned char", // h
+  WChar = "wchar_t", // w
+  Short = "short", // s
+  UShort = "unsigned short", // t
+  Int = "int", // i
+  UInt = "unsigned int", // j
+  Long = "long", // l
+  ULong = "unsigned long", // m
+  LongLong = "long long", // x
+  ULongLong = "unsigned long long", // y
+  Int128 = "__int128", // n
+  UInt128 = "unsigned __int128", // o
+  Float = "float", // f
+  Double = "double", // d
+  LongDouble = "long double", // e
+  Float128 = "__float128", // g
+
+  Member = "member", // TODO: Not sure when this is used, id is "M"
+  Imaginary = "imaginary", // TODO: Research "imaginary type qualifier (C 2000)". id is "G"
+  Complex = "complex", // TODO: Research "complex type qualifier (C 2000)". id is "H"
+  Restrict = "restrict", // TODO: Research "restrict qualifier (C 2000)". id is "r"
+  VendorExtendedType = "vendor-extended-type", // TODO: Research "vendor extended builtin type". id is "u"
+  VendorExtendedQualifier = "vendor-extended-qualifier", // TODO: Research "vendor extended type qualifier". id is "U"
+  ExternC = "extern-c", // TODO: When is this used in name mangling? id is "Y"
+
+  Array = "array", // A
+  Ellipsis = "...", // z
+  PackExpansion = "...T", // Dp
+  Decltype2 = "decltype2", // Dt
+  Decltype = "decltype", // DT
+
+  ERROR = "error",
+}
+const TypeMapping: { [k: string]: Type } = {
+  v: Type.Void,
+  b: Type.Bool,
+  c: Type.Char,
+  a: Type.SChar,
+  h: Type.UChar,
+  w: Type.WChar,
+  s: Type.Short,
+  t: Type.UShort,
+  i: Type.Int,
+  j: Type.UInt,
+  l: Type.Long,
+  m: Type.ULong,
+  x: Type.LongLong,
+  y: Type.ULongLong,
+  n: Type.Int128,
+  o: Type.UInt128,
+  f: Type.Float,
+  d: Type.Double,
+  e: Type.LongDouble,
+  g: Type.Float128,
+
+  M: Type.Member,
+  G: Type.Imaginary,
+  H: Type.Complex,
+  r: Type.Restrict,
+  u: Type.VendorExtendedType,
+  U: Type.VendorExtendedQualifier,
+  Y: Type.ExternC,
+
+  A: Type.Array,
+  z: Type.Ellipsis,
+  Dp: Type.PackExpansion,
+  Dt: Type.Decltype2,
+  DT: Type.Decltype,
+};
+
+class Argument {
+  public static parse(str: string): { arg: Argument | null; consumed: number } {
+    const arg = new Argument(str);
+    if (arg.type === Type.ERROR) {
+      return { arg: null, consumed: 0 };
+    }
+    return { arg, consumed: arg.consumend };
+  }
+
+  private constructor(str: string) {
+    const keys = Object.keys(TypeMapping);
+    for (const key of keys) {
+      if (str.startsWith(key)) {
+        this.type = TypeMapping[key];
+        this.consumend = key.length;
         break;
-      }
-      default: {
-        return { consumed: i, value: ret };
       }
     }
   }
-  return { consumed: str.length, value: ret };
-}
 
-class Substitution {
-  values: string[] = [];
+  private child: Argument | null = null;
+  private qualifiers: Qualifier[] = [];
+  private type: Type = Type.ERROR;
+  private consumend: number = 0;
 
-  push(sub: string) {
-    this.values.push(sub);
-  }
-
-  pop(): string | undefined {
-    const val = this.values.pop();
-    return val;
-  }
-
-  count() {
-    return this.values.length;
-  }
-
-  /*
-    S_ is first
-    S_0 is second
-    S_1 is third
-    ...
-  */
-  get(idx: "_" | number) {
-    return idx === "_" ? this.values[0] : this.values[idx + 1];
+  public toString(): string {
+    return this.type;
   }
 }
 
-enum Modifiers {
-  Pointer = "P",
-  Const = "K",
-  Reference = "R",
+enum Qualifier {
+  Pointer = "*", // P
+  Const = "const ", // K
+  Volatile = "volatile ", // V
+  Reference = "&", // R
+  RValueReference = "&&", // O
 }
+
+// enum AbreviationTypes {
+//   StdAllocator = "std::allocator", // Sa
+//   StdBasicString = "std::basic_string", // Sb
+//   StdIOStream = "std::basic_iostream<char,std::char_traits<char>>", // Sd
+//   StdIStream = "std::basic_istream<char,std::char_traits<char>>", // Si
+//   StdOStream = "std::basic_ostream<char,std::char_traits<char>>", // So
+//   StdString = "std::basic_string<char,std::char_traits<char>,std::allocator<char>>", // Ss
+// }
+// const AbreviationTypesMapping: { [k: string]: AbreviationTypes } = {
+//   Sa: AbreviationTypes.StdAllocator,
+//   Sb: AbreviationTypes.StdBasicString,
+//   Sd: AbreviationTypes.StdIOStream,
+//   Si: AbreviationTypes.StdIStream,
+//   So: AbreviationTypes.StdOStream,
+//   Ss: AbreviationTypes.StdString,
+// };
 
 export function isMangled(symbol: string) {
   return symbol.startsWith("_Z") || symbol.startsWith("__Z");
@@ -84,77 +202,80 @@ export function demangle(input: string): string {
   const isConst = symbol[0] === "L";
   if (isConst) symbol = symbol.substring(1);
 
-  const namespacingActive = symbol[0] === "N";
-  if (namespacingActive) symbol = symbol.substring(1);
-
   const namespaceParts: string[] = [];
-  while (symbol.length > 0) {
-    const appreviation = symbol.substring(0, 2);
-    switch (appreviation) {
-      case "St":
+  let name: string = "";
+
+  // Namespacing active
+  const isNamespaceEscaped = symbol[0] === "N";
+  const isInStdNamespace = symbol.startsWith("St");
+  if (isNamespaceEscaped || isInStdNamespace) {
+    if (isNamespaceEscaped) symbol = symbol.substring(1);
+
+    while (symbol.length > 0) {
+      if (symbol.startsWith("St")) {
+        symbol = symbol.substring(2);
         namespaceParts.push("std");
-        symbol = symbol.substring(2);
-        continue;
-      case "Sa":
-        namespaceParts.push("std::allocator");
-        symbol = symbol.substring(2);
-        continue;
-      case "Sb":
-        namespaceParts.push("std::basic_string");
-        symbol = symbol.substring(2);
-        continue;
-      case "Ss":
-        namespaceParts.push(
-          "std::basic_string<char,::std::char_traits<char>,::std::allocator<char>>"
-        );
-        symbol = symbol.substring(2);
-        continue;
-      case "Si":
-        namespaceParts.push(
-          "std::basic_istream<char, ::std::char_traits<char> >"
-        );
-        symbol = symbol.substring(2);
-        continue;
-      case "So":
-        namespaceParts.push(
-          "std::basic_ostream<char,::std::char_traits<char>>"
-        );
-        symbol = symbol.substring(2);
-        continue;
-      case "Sd":
-        namespaceParts.push(
-          "std::basic_iostream<char,::std::char_traits<char>>"
-        );
-        symbol = symbol.substring(2);
-        continue;
-      default:
+        continue; // std::std:: is cursed but valid. Would it be mangled like that though?
+      }
+      const part = readLengthEncoded(symbol);
+      if (part.consumed === 0) {
         break;
+      }
+      namespaceParts.push(part.value);
+      symbol = symbol.substring(part.consumed);
     }
-    const partLen = readDenary(symbol);
-    if (isNaN(partLen.value)) {
+
+    if (isNamespaceEscaped) {
+      if (symbol[0] !== "E" || namespaceParts.length === 0) {
+        return input;
+      }
+      symbol = symbol.substring(1);
+    }
+
+    name = namespaceParts.pop() as string; // Check above for length, so cannot return undefined
+  } else {
+    const namePart = readLengthEncoded(symbol);
+    if (namePart.consumed === 0) {
+      return input;
+    }
+    name = namePart.value;
+    symbol = symbol.substring(namePart.consumed);
+    // Is in a namespace, but not encoded with N...E
+    if (!isNaN(readDenary(symbol).value)) {
+      return input;
+    }
+  }
+
+  // Variable
+  if (symbol.length === 0) {
+    let demangled = isConst ? "const " : "";
+    for (const part of namespaceParts) {
+      demangled += part + "::";
+    }
+    demangled += name;
+    return demangled;
+  }
+
+  let argumentTypes: Argument[] = [];
+  while (symbol.length > 0) {
+    const { arg, consumed } = Argument.parse(symbol);
+    if (consumed === 0 || arg === null) {
       break;
     }
-    symbol = symbol.substring(partLen.consumed);
-    if (symbol.length < partLen.value) {
-      return input;
-    }
-    const part = symbol.substring(0, partLen.value);
-    namespaceParts.push(part);
-    symbol = symbol.substring(partLen.value);
+    argumentTypes.push(arg);
+    symbol = symbol.substring(consumed);
   }
-  const name = namespaceParts.pop();
-  if (namespacingActive) {
-    if (symbol[0] !== "E") {
-      return input;
-    }
-    symbol = symbol.substring(1);
+
+  if (argumentTypes.length === 0) {
+    return input;
   }
 
   let demangled = isConst ? "const " : "";
   for (const part of namespaceParts) {
     demangled += part + "::";
   }
-  demangled += name;
-
+  demangled += name + "(";
+  demangled += argumentTypes.join(",");
+  demangled += ")";
   return demangled;
 }
